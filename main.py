8ed93828f1b3ac28e3e0d6ab86ac71761d11dfc6
prefect12 <jqmhpj@gmail.com>
2022-01-03 15:55:21 +0800

    update mouse

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import time
 import win32api, win32con, win32gui
+import mouse
 
 class PhotoRecognize:
     def __init__(self,imgPath):
@@ -30,20 +31,41 @@ class PhotoRecognize:
 
 class MyMouse:
     def __init__(self):
-        pass
+        self.m = mouse
 
-    def draw(self):
-        pass
+    def draw(self,func):
+        def warpFunc():
+            self.hold()
+            func()
+            self.release()
+        return warpFunc
 
-    def moveTo(self):
-        pass
+    def moveToLeft(self,distance):
+        self.draw(self.move(-distance,0))()
 
-    def click(self):
-        pass
+    def moveToRight(self,distance):
+        self.draw(self.move(distance, 0))()
+
+    def moveToUp(self,distance):
+        self.draw(self.move(0, distance))()
+
+    def moveToDown(self,distance):
+        self.draw(self.move(0, -distance))()
+
+    def move(self,x,y):
+        self.m.move(x,y)
+
+    def leftClick(self):
+        self.m.click('left')
 
     def hold(self):
-        pass
+        self.m.press('left')
 
+    def getPostion(self):
+        return self.m.get_position()
+
+    def release(self):
+        self.m.release('left')
 
 class WindowManipulater:
     def __init__(self,name,screenShotPath):
